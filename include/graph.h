e9124c5a8e7ed76a6456c53ceca5cf111c61bd35
CalebCintary <calebcintaryy@gmail.com>
2022-03-15 17:57:15 +0300

    Adding base set of cmake configuration and basic graph class

---
+++ include/graph.h
@@ -0,0 +1,15 @@
+#ifndef PYPLOT_CPP_GRAPH_H
+#define PYPLOT_CPP_GRAPH_H
+
+namespace pyplot {
+
+    class Graph {
+
+    };
+
+}
+
+
+
+
+#endif //PYPLOT_CPP_GRAPH_H
---
+++ include/pyplot_cpp.h
@@ -0,0 +1,6 @@
+#ifndef PYPLOT_CPP_PYPLOT_CPP_H
+#define PYPLOT_CPP_PYPLOT_CPP_H
+
+
+
+#endif //PYPLOT_CPP_PYPLOT_CPP_H
