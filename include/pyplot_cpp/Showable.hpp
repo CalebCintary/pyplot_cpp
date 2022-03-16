79c44346340e8817c6d0e66cda890aae7f4f1e28
CalebCintary <calebcintaryy@gmail.com>
2022-03-17 00:21:03 +0300

    Forgeted changes and License update

---
+++ include/pyplot_cpp/Showable.hpp
@@ -0,0 +1,17 @@
+//
+// Created by calebcintary on 3/17/22.
+//
+
+#ifndef PYPLOT_CPP_SHOWABLE_HPP
+#define PYPLOT_CPP_SHOWABLE_HPP
+
+namespace pyplot_cpp {
+    class Showable {
+
+    };
+}
+
+
+
+
+#endif //PYPLOT_CPP_SHOWABLE_HPP
---
+++ src/Showable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by calebcintary on 3/17/22.
+//
+
+#include "pyplot_cpp/Showable.hpp"
