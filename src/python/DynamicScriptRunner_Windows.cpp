baaf819640946fc18730561153efb87ff2f8dcb6
CalebCintary <calebcintaryy@gmail.com>
2022-03-26 16:19:33 +0300

    Added Windows Threads
    Signed-off-by: CalebCintary <calebcintaryy@gmail.com>

--- src/python/DynamicScriptRunner_Windows.cpp
+++ src/python/DynamicScriptRunner_Windows.cpp
@@ -4,7 +4,33 @@
 
 #include "pyplot_cpp/python/DynamicScriptRunner.hpp"
 
+#include <Windows.h>
+
+DWORD WINAPI thread(LPVOID param) {
+    PythonRunner_Threads_Start(param);
+    return 0;
+}
+
+
 void pyplot_cpp::python::DynamicScriptRunner::Run(bool async) {
-    // FixMe
+    std::ofstream out;
+    std::string fileName =
+            "pyplot_cpp_tmp_" +
+            std::to_string(std::chrono::high_resolution_clock::now().time_since_epoch().count()) +
+            ".py";
+//    out.open("python_file.py");
+    out.open(fileName);
+
+    out << script->getCode();
+    out.close();
+
+    if (async) {
+        DWORD threadId;
+        HANDLE handle = CreateThread(NULL, NULL, thread, &fileName, 0, &threadId);
+    } else {
+        PythonRunner_Threads_Start(&fileName);
+    }
+
+    remove(fileName.c_str());
 }
 
